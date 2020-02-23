fe11875d3ac828bf3dcc58a8a0ffefcb0924ce94
0994486@gmail.com <6234Ai1949sa>
2020-02-23 06:56:02 +0300

    Выполняет задание "На пути к совершенству" (все заново)

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
 
-function debug($data)
-{
-    echo '<pre>' . print_r($data, true) . '</pre>';
+require_once __DIR__ . '/vendor/autoload.php';
+
+use TaskForce\Tasks\Status;
+
+$followingStatus = 'При выборе действия "%1s" задание перейдёт в статус "%2s"';
+foreach (Status::getActions() as $action) {
+    assert(in_array(Status::getFollowingStatus($action), Status::getStatuses()));
+    printf($followingStatus, $action::getName(), Status::getFollowingStatus($action));
+    print '<br>';
 }
 
-require_once './vendor/autoload.php';
+print '<br>';
+$customerId = 1;
+$executorId = 2;
+$termExecution = '2020-01-31';
 
-use app\Task\Task;
-use app\Action\{Action, AvailableActions};
-
-$task = new Task(['authorId' => 1, 'executorId' => 2, 'status' => Task::STATUS_EXECUTION]);
-
-AvailableActions::setData($task, 1);
-debug(AvailableActions::getAvailableActions());
-debug(AvailableActions::getNextStatus(Action::ACTION_CANCELED));
+$userId = 1;
+$userRole = 'customer';
+$currentStatus = 'new';
+$statusOne = new Status($customerId, $executorId, $termExecution, $currentStatus);
+assert(in_array(Status::ACTION_CANCEL, $statusOne->getAvailableActions($userId, $userRole)));
+assert(in_array(Status::ACTION_WORK, $statusOne->getAvailableActions($userId, $userRole)));
+print 'Статус задания - Новое, Пользователь - автор задания, Статус пользователя - Заказчик, <br>
+Доступные действия: ' . implode(', ', $statusOne->getAvailableActions($userId, $userRole));
 
